591471fd6578b6c9f7f07ff758952255ff941f06
shorty <ungegrieftfirma@gmail.com>
2023-12-31 00:51:47 +0100

    intelli code thing

--- src/main/java/net/uniquepixels/core/velocity/messaging/MessageChannel.java
+++ src/main/java/net/uniquepixels/core/velocity/messaging/MessageChannel.java
@@ -1,9 +1,11 @@
 package net.uniquepixels.core.velocity.messaging;
 
 import com.velocitypowered.api.event.connection.PluginMessageEvent;
+import org.intellij.lang.annotations.Subst;
 
 public interface MessageChannel {
 
+    @Subst("")
     String id();
 
     void event(PluginMessageEvent event);
