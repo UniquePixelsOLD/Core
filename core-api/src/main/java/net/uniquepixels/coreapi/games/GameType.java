34f9765d7591cb6c14e80eef02ef137da2e7a846
shorty <ungegrieftfirma@gmail.com>
2024-01-02 04:58:38 +0100

    moved and renamed

---
+++ core-api/src/main/java/net/uniquepixels/coreapi/games/GameType.java
@@ -0,0 +1,9 @@
+package net.uniquepixels.coreapi.games;
+
+public enum GameType {
+
+    MANHUNT,
+    THE_LUCK,
+    HIDE_AND_SEEK
+
+}
--- src/main/java/net/uniquepixels/core/paper/games/GameTypes.java
+++
@@ -1,9 +0,0 @@
-package net.uniquepixels.core.paper.games;
-
-public enum GameTypes {
-
-    MANHUNT,
-    THE_LUCK,
-    HIDE_AND_SEEK
-
-}
