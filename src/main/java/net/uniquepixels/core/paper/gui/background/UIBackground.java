7d7bcbc34b836b2447b794b131769fea2afd72a3
shorty <ungegrieftfirma@gmail.com>
2024-03-13 20:15:28 +0100

    feat(simplify default ui background): added method to create an default ui background
    the added method creates an UIBackground object, that can be used to set the default background of an ui to "all gray stained glass panes" (without text and item flags)

--- src/main/java/net/uniquepixels/core/paper/gui/background/UIBackground.java
+++ src/main/java/net/uniquepixels/core/paper/gui/background/UIBackground.java
@@ -1,19 +1,30 @@
 package net.uniquepixels.core.paper.gui.background;
 
 
+import net.kyori.adventure.text.Component;
+import net.uniquepixels.core.paper.gui.UISlot;
 import net.uniquepixels.core.paper.gui.item.UIItem;
+import net.uniquepixels.core.paper.item.DefaultItemStackBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
 
 import java.util.List;
 
 public record UIBackground(BackgroundType type, List<UIItem> backgroundItems) {
 
+    public static UIBackground createDefaultBackground() {
+        return new UIBackground(BackgroundType.FULL, List.of(new UIItem(new DefaultItemStackBuilder<>(Material.GRAY_STAINED_GLASS_PANE)
+                .displayName(Component.empty())
+                .addFlags(ItemFlag.values())
+                .buildItem(), UISlot.SLOT_4)));
+    }
 
 
-  public enum BackgroundType {
-    FULL,
-    NONE,
-    SELF,
-    BOTTOM_LINE
-  }
+    public enum BackgroundType {
+        FULL,
+        NONE,
+        SELF,
+        BOTTOM_LINE
+    }
 
 }
